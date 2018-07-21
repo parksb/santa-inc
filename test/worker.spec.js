e0f35b026498fdd41d9b1f91ab67f77358168fc9
ParkSB <parkgds@gmail.com>
2018-07-22 03:31:12 +0900

    test: worker 테스트 - getName() - getOutput()

---
+++ test/worker.spec.js
@@ -0,0 +1,35 @@
+import Rudolph from '../src/workers/Rudolph';
+import InternElf from '../src/workers/InternElf';
+import RegularElf from '../src/workers/RegularElf';
+import Machine from '../src/workers/Machine';
+import CyborgElf from '../src/workers/CyborgElf';
+import Couple from '../src/workers/Couple';
+
+const rudolph = new Rudolph();
+const internElf = new InternElf();
+const regularElf = new RegularElf();
+const machine = new Machine();
+const cyborgElf = new CyborgElf();
+const couple = new Couple();
+
+describe('getName()', () => {
+  it('should returns the name of the worker', () => {
+    expect(rudolph.getName()).toBe('rudolph');
+    expect(internElf.getName()).toBe('internElf');
+    expect(regularElf.getName()).toBe('regularElf');
+    expect(machine.getName()).toBe('machine');
+    expect(cyborgElf.getName()).toBe('cyborgElf');
+    expect(couple.getName()).toBe('couple');
+  });
+});
+
+describe('getOutput()', () => {
+  it('should returns a value between minOutput and maxOutput', () => {
+    expect(rudolph.getOutput()).toBeWithinRange(1, 2);
+    expect(internElf.getOutput()).toBeWithinRange(2, 5);
+    expect(regularElf.getOutput()).toBeWithinRange(2, 7);
+    expect(machine.getOutput()).toBeWithinRange(4, 10);
+    expect(cyborgElf.getOutput()).toBeWithinRange(6, 12);
+    expect(couple.getOutput()).toBeWithinRange(9, 15);
+  }); 
+});
