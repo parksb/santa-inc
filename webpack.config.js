02787e19fd7b92b904ef445390c0717e2a35fa0d
ParkSB <parkgds@gmail.com>
2018-07-31 21:27:40 +0900

    chore: babel-eslint 설치 - babel-preset가 폐지되었으므로 다시 제거해야 함.

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,7 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['env']
+            presets: ['env', 'stage-3']
           }
         }
       }
