ec7d3ece7b1d3dd3b903fd5a3eeaba16737bf40e
ParkSB <parkgds@gmail.com>
2018-07-31 21:28:43 +0900

    feat: policy abstract class 내용 구성

--- src/policies/policy.js
+++ src/policies/policy.js
@@ -1,5 +1,41 @@
 class Policy {
   constructor() {
-    // TODO: implement this
+    this.cost = 0;
+
+    this.name = '';
+    this.korName = '';
+
+    this.description = '';
+    this.spec = '';
+  }
+
+  getCost() {
+    return this.cost;
+  }
+
+  getName() {
+    return this.name;
+  }
+
+  getKorName() {
+    return this.korName;
+  }
+
+  getDescription() {
+    return this.description;
+  }
+
+  getSpec() {
+    return this.spec;
+  }
+
+  next() {
+    throw new Error('next() must be implemented.');
+  }
+
+  execute() {
+    throw new Error('execute() must be implemented.');
   }
 }
+
+export default Policy;
