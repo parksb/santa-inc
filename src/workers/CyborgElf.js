acd35ad73717210d57f7c7d12e9b8df44ca5af92
ParkSB <parkgds@gmail.com>
2018-10-28 16:26:06 +0900

    feat: 사이보그 요정을 요정봇으로 변경

--- src/workers/CyborgElf.js
+++ src/workers/CyborgElf.js
@@ -12,7 +12,7 @@ class CyborgElf extends Worker {
     super();
 
     this.name = 'cyborgElf';
-    this.korName = '사이보그 요정';
+    this.korName = '요정봇';
 
     this.img = '/assets/cyborg.gif';
   }
