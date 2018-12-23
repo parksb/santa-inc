713ea88923fa411b5a47804135df90ed47bb9043
ParkSB <parkgds@gmail.com>
2018-12-23 11:43:30 +0900

    refactor: 직원, 정책 테이블 분리

---
+++ src/data/WorkerTable.ts
@@ -0,0 +1,14 @@
+import * as W from '../workers/index';
+
+const WorkerTable = {
+  rudolph: W.Rudolph,
+  parttimeElf: W.ParttimeElf,
+  internElf: W.InternElf,
+  regularElf: W.RegularElf,
+  machine: W.Machine,
+  cyborgElf: W.CyborgElf,
+  tree: W.Tree,
+  children: W.Children,
+}
+
+export default WorkerTable;
